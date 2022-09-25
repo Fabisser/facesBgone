77f929ebd4f29f6cd2fefbecd7ce674ad4fbc8c5
Fabisser <79523968+Fabisser@users.noreply.github.com>
2022-09-26 00:17:41 +0200

    Add files via upload

---
+++ build/CMakeFiles/convex_hull.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/convex_hull.dir/src/main.cpp.o"
+  "CMakeFiles/convex_hull.dir/src/main.cpp.o.d"
+  "convex_hull.cpython-310-x86_64-linux-gnu.so"
+  "convex_hull.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/convex_hull.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/convex_hull.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for convex_hull.
+# This may be replaced when dependencies are built.
