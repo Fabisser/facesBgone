eac9b65cb347fc86e5f18a7b5e16dfebc14ad47f
Fabisser <79523968+Fabisser@users.noreply.github.com>
2022-09-28 10:37:14 +0200

    Removed files to see if it works without build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/fabzv/Desktop/Delft/Synthesis-Project
-# Build directory: /Users/fabzv/Desktop/Delft/Synthesis-Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("pybind11")
