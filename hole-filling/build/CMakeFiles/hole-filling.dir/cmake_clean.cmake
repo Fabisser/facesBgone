9e3c725925edf851b664e0598ae46b99577dde6a
Fabisser <79523968+Fabisser@users.noreply.github.com>
2022-11-04 10:46:23 +0100

    Renamed the hole-filling project

---
+++ hole-filling/build/CMakeFiles/hole-filling.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hole-filling.dir/src/main.cpp.o"
+  "CMakeFiles/hole-filling.dir/src/main.cpp.o.d"
+  "hole-filling"
+  "hole-filling.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hole-filling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
