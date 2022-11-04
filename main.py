a72f226599d1d3e8c84ba2547edd8bc61472e2bf
Fabisser <79523968+Fabisser@users.noreply.github.com>
2022-11-04 10:12:24 +0100

    Clearing file for main

--- main.py
+++
@@ -1,8 +0,0 @@
-from pycode import *
-import build.convex_hull
-
-print("Calculating Adjacancy")
-path = "pycode/blocks_of_adjacent_buildings_subset_5907.json"
-list = common_walls(path)
-
-build.convex_hull.calculate(list)
