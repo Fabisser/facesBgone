b076d5f37e651a2aa40ad506e040539fb5bc8b61
Fabisser <79523968+Fabisser@users.noreply.github.com>
2022-11-09 11:42:52 +0100

    minor changes to bash

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 Help()
 {
    # Display Help
-
+   echo ""
    echo "facesBgone removes faces between adjacent buildings from a CityJSON dataset in LoD 2.2 (e.g. 3D BAG)."
    echo "For more information, refer to README.md."
    echo
@@ -20,13 +20,13 @@ Help()
    echo "		default c = 0.1"
    echo
    echo "Hole filling parameters:"
-   echo "	hole	maximum diameter of holes to be filled."
+   echo "	d	maximum diameter of holes to be filled."
    echo "		default hole = -1"
    echo "	e	maximum number of edges of holes to be filled."
    echo "		default e = -1"
    echo
    echo "Nef Polyhedra parameters:"
-   echo "	mink	value used for minkowski sum."
+   echo "	p	value used for minkowski sum."
    echo "		default mink = 0.003"
    
 }
@@ -40,18 +40,17 @@ while getopts ":hm:i:c:mink:hole:e:" option; do
 	m) method=$OPTARG;;
 	i) inter_area=$OPTARG;;
 	c) cluster_meshes=$OPTARG;;
-	mink) minkowski=$OPTARG;;
-	hole) max_hole=$OPTARG;;
+	p) minkowski=$OPTARG;;
+	d) max_hole=$OPTARG;;
 	e) max_edges=$OPTARG;;
    esac
 done
 
 curdir="$(pwd)"
-filename=$1
 
-# # get inputs from user
-# echo "Enter CityJson File Name: ";
-# read filename;
+# get inputs from user
+echo "Enter CityJson File Name: ";
+read filename;
 
 if [ -z $filename ];
 then
@@ -62,14 +61,10 @@ else
 fi
 
 # 1. adjacency
-start=`gdate +%s%N`
 
 cd ./adjacency
 python3 main.py $filename "${inter_area:-$default_inter_area}" "${cluster_meshes:-$default_cluster_meshes}"
 
-end=`gdate +%s%N`
-runtime=$((end-start))
-echo "$runtime"
 if [ -z $minkowski]
 then
 	minkowski=0.01
@@ -83,20 +78,14 @@ then
 	
 	cd ../hole-filling/build/
 	chmod a+x hole-filling
-	start1=`gdate +%s%N`
 	./hole-filling $curdir
-	end1=`gdate +%s%N`
-	runtime1=$((end1-start1))
-	echo "$runtime1"
+
 
 
 	cd ../../nef/build/
 	chmod a+x geoCFD
-	start2=`gdate +%s%N`
 	./geoCFD $filename $curdir/data $minkowski
-	end2=`gdate +%s%N`
-	runtime2=$((end2-start2))
-	echo "$runtime2"
+
 
 else
     echo "Your method of choice is: $method"
